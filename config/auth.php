565e259f79ae07b04cc5a60eeb5da45cdc31977c
RabieImghi <rabieaitimghi7@gmail.com>
2024-02-28 10:06:20 +0100

    add database

--- config/auth.php
+++ config/auth.php
@@ -67,6 +67,7 @@ return [
     'providers' => [
         'users' => [
             'driver' => 'eloquent',
+            'model' => App\Models\User::class,
         ],
 
         // 'users' => [
--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 Route::get('/', function () {
-    return view('admin.register');
+    return view('admin.category');
 });
 
 Route::post('/login', 'App\Http\Controllers\AuthController@login');
